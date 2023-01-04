ab0dec9f1b432696698a2766d1299170473c3dbb
rabosian <msann1356@gmail.com>
2023-01-03 23:39:08 -0500

    next.config.js - rewrite, redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -39,11 +39,7 @@ export default function Home({ results }) {
 }
 
 export async function getServerSideProps() {
-  const { results } = await (
-    await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US`
-    )
-  ).json();
+  const { results } = await (await fetch("http://localhost:3000/api/movies")).json();
   return {
     props: {
       results,
