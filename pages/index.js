ff3478400b51d7b646b125c5c34b3202e1592f08
rabosian <msann1356@gmail.com>
2023-01-04 13:34:49 -0500

    passing query object with next/link

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,30 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Home({ results }) {
-  const router = useRouter();
-  const navigate = (id, title) => {
-    router.push({
-      pathname: `/movies/${id}`,
-      query: {
-        title
-      },
-    }, `/movies/${id}`) // hide query from URL using as option
-  };
-
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        // <Link href={`/movies/${movie.id}`} key={movie.id}>
-        <div
-          onClick={() => navigate(movie.id, movie.original_title)}
-          className="movie"
+        <Link
+          href={{
+            pathname: `/movies/${movie.id}`,
+            query: {
+              title: movie.original_title,
+            },
+          }}
+          as={`/movies/${movie.id}`}
           key={movie.id}
         >
-          <img
-            src={`https://www.themoviedb.org/t/p/w440_and_h660_face${movie.poster_path}`}
-          />
-          <h4>{movie.original_title}</h4>
-        </div>
-        // </Link>
+          <div
+            className="movie"
+            key={movie.id}
+          >
+            <img
+              src={`https://www.themoviedb.org/t/p/w440_and_h660_face${movie.poster_path}`}
+            />
+            <h4>{movie.original_title}</h4>
+          </div>
+        </Link>
       ))}
 
       <style jsx>{`
